7bca827a7f96e3b2d191f615cce37687bf733ea9
Serjml <Serjml@list.ru>
2022-04-27 15:46:59 +0300

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  sampleActivity = +sampleActivity;
+  if (sampleActivity < 0) return false;
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t =  Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+  if (t < 0) return false;
+  if (t === Infinity) return false;
+  if (isNaN(t)) return false;
+  return t;
 }
 
 module.exports = {
