d68e86bdaf4a7dd2d578daffc00ac00bfbbc13ad
Serjml <Serjml@list.ru>
2022-04-27 14:17:24 +0300

    feat: solve count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(countCats) {
+  let count = 0;
+  for (let i = 0; i < countCats.length; i++) {
+    for (let j = 0; j < countCats[i].length; j++) {
+      if (countCats[i][j] === '^^') {
+        count++
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = {
