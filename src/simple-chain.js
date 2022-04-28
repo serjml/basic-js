67439d216f00a6b62d036ad78a4fb9d7f905c918
Serjml <Serjml@list.ru>
2022-04-28 13:15:43 +0300

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,27 +4,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  array: [],
+  
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === undefined) {
+      this.array.push('( )');
+    } else {
+      this.array.push(`( ${value} )`);
+    }    
+    return this  
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(pos) {
+    if ((typeof pos !== 'number') || (pos <= 0) || (pos > this.array.length)) {
+      this.array = [];
+      throw new Error("You can't remove incorrect link!");
+    } else {      
+      this.array.splice(pos - 1, 1);      
+    }    
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let string = this.array.join('~~');
+    this.array = [];
+    return string;
+  }, 
 };
 
 module.exports = {
