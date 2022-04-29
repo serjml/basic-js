ba361859d518b1aab0124b7e4843744694a8fc16
Serjml <Serjml@list.ru>
2022-04-29 12:01:13 +0300

    feat: solve mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function minesweeper(matrix) {
+  let mat = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  
+  for (i = 0; i < matrix.length; i++) {
+     for (j = 0; j < matrix[i].length; j++) {      
+       if (matrix[i][j] === true) {      
+         if ((i == 0) && (j == 0)) {          
+           mat[i][j+1]++;
+           mat[i+1][j]++;
+           mat[i+1][j+1]++;          
+         } else if ((i == matrix.length - 1) && (j == matrix[i].length - 1)) {
+           mat[i-1][j-1]++;
+           mat[i-1][j]++;
+           mat[i][j-1]++; 
+         } else if ((i == 0) && (j == matrix[i].length - 1)) {
+           mat[i][j-1]++;        
+           mat[i+1][j-1]++;
+           mat[i+1][j]++;         
+         } else if ((i == matrix.length - 1) && (j == 0)) {
+           mat[i-1][j]++;
+           mat[i-1][j+1]++;                 
+           mat[i][j+1]++;
+         } else if (i == 0) {
+           mat[i][j-1]++;        
+           mat[i][j+1]++;
+           mat[i+1][j-1]++;
+           mat[i+1][j]++;
+           mat[i+1][j+1]++;
+         } else if ((j == 0)) {
+            mat[i-1][j]++;
+            mat[i-1][j+1]++;
+            mat[i][j+1]++;
+            mat[i+1][j]++;
+            mat[i+1][j+1]++;
+         } else if (i == matrix.length - 1) {
+           mat[i-1][j-1]++;
+           mat[i-1][j]++;
+           mat[i-1][j+1]++;
+           mat[i][j-1]++;        
+           mat[i][j+1]++;
+         } else if (j == matrix[i].length - 1) {
+           mat[i-1][j-1]++;
+           mat[i-1][j]++;
+           mat[i][j-1]++;
+           mat[i+1][j-1]++;
+           mat[i+1][j]++;
+         } else {
+           mat[i-1][j-1]++;
+           mat[i-1][j]++;
+           mat[i-1][j+1]++;
+           mat[i][j-1]++;        
+           mat[i][j+1]++;
+           mat[i+1][j-1]++;
+           mat[i+1][j]++;
+           mat[i+1][j+1]++;
+         } 
+       }      
+     }
+ } 
+ if (matrix.length - mat.length) {
+  mat.splice(matrix.length - mat.length)
+} 
+   return mat
+ }
 
 module.exports = {
   minesweeper
