7cdb27f6438313fb7fc015f275ade8fdcb62bc88
Serjml <Serjml@list.ru>
2022-05-01 12:51:36 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,88 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direction = true) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+ 
+    string = string.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+  
+    while(key.length < string.length) {
+      key.push(...key);
+    } 
+    
+    for(i = 0; i < string.length; i++) {
+      if ((string[i].charCodeAt() < 65) || (string[i].charCodeAt() > 90)) {
+        key.splice(i, 0, string[i])      
+      }   
+    }
+    key = key.slice(0, string.length);
+  
+    key = key.map(el => el.charCodeAt());
+    string = string.map(el => el.charCodeAt());
+  
+     for(i = 0; i < string.length; i++) {
+       if (string[i] < 65 || string[i] > 90) {       
+        continue;
+       } else {
+         string[i] += key[i] - 65;
+         if (string[i] > 90) {
+           string[i] = string[i] - 26;
+         }
+       }
+     }  
+   
+    if (this.direction) {
+      return string = string.map(el => String.fromCharCode(el)).join('');
+    } else {
+      return string = string.map(el => String.fromCharCode(el)).reverse().join('');
+    }
+  }
+
+  decrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+ 
+    string = string.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+  
+    while(key.length < string.length) {
+      key.push(...key);
+    } 
+    
+    for(i = 0; i < string.length; i++) {
+      if ((string[i].charCodeAt() < 65) || (string[i].charCodeAt() > 90)) {
+        key.splice(i, 0, string[i])      
+      }   
+    }
+    key = key.slice(0, string.length);
+  
+    key = key.map(el => el.charCodeAt());
+    string = string.map(el => el.charCodeAt());
+  
+     for(i = 0; i < string.length; i++) {
+       if (string[i] < 65 || string[i] > 90) {       
+        continue;
+       } else {
+         string[i] = string[i] - key[i] + 65;
+         if (string[i] < 65) {
+           string[i] = string[i] + 26;
+         }
+       }
+     }  
+   
+    if (this.direction) {
+      return string = string.map(el => String.fromCharCode(el)).join('');
+    } else {
+      return string = string.map(el => String.fromCharCode(el)).reverse().join('');
+    }
   }
 }
 
